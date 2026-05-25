984ce515d094036520644bb344067efa69bb3870
Afham <afhamtab@gmail.com>
2026-05-25 15:38:17 +0530

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,7 @@
+name = input("Enter Username: ")
+password = input("Enter Password: ")
+
+if name == "admin" and password == "1234":
+    print("Welcome to python")
+else:
+    print("Invalid Credintional")
